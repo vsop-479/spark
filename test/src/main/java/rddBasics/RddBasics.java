1f53a00e2d44702727f50d34d5b7d741904c529a
zhouhui <zhouhui@360.net>
2018-06-04 18:40:36 +0800

    spark

---
+++ test/src/main/java/rddBasics/RddBasics.java
@@ -0,0 +1,25 @@
+package rddBasics;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+
+public class RddBasics {
+    public static void main(String[] args) {
+        //        D:/hadoop/bin/winutils.exe
+        System.setProperty("hadoop.home.dir", "D:\\hadoop");
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("RddBasics");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        //a external dataset
+        JavaRDD<String> lines = sparkContext.textFile("D:\\escluster.txt");
+        //transformation to a new dataset
+        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+        //persist lineLengths
+        lineLengths.persist(StorageLevel.MEMORY_ONLY());
+        //action to a value
+        Integer totalLength = lineLengths.reduce((a, b) -> a + b);
+        System.out.println(totalLength);
+    }
+}
