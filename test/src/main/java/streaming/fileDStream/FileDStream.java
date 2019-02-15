3727d8f5936fe2aa6a2e817839fbff86a9658e81
zhouhui <zhouhui@360.net>
2019-02-15 19:04:27 +0800

    file dStream.

--- test/src/main/java/streaming/fileDStream/FileDStream.java
+++ test/src/main/java/streaming/fileDStream/FileDStream.java
@@ -1,35 +1,27 @@
-package streaming.tcpDStream;
+package streaming.fileDStream;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
 
-/**
- * Created by zhouhui on 2019/1/6.
- */
-public class TcpDStream {
+public class FileDStream {
+
     public static void main(String[] args){
         //        D:/hadoop/bin/winutils.exe
         System.setProperty("hadoop.home.dir", "D:\\hadoop");
-//        local[2]:local:a local StreamingContext , 2:two working thread.
+        //        local[2]:local:a local StreamingContext , 2:two working thread.
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
-//        1:batch interval of 1 second.
-        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(1));
+        //        1:batch interval of 1 second.
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(5));
 
-//Create a DStream that will connect to hostname:port, like localhost:9999.
-//        lines:the stream of data that will be received from the data server.
-        String host = "localhost";
-//        host = "10.95.134.109";
-        JavaReceiverInputDStream<String> lines = streamingContext.socketTextStream(host, 9999);
-        System.out.println("lines============");
-        lines.print();
-//Split each line into words.
+        String directory = "E:\\problem\\connect";
+        JavaDStream<String> lines = streamingContext.textFileStream(directory);
+        //Split each line into words.
         JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
 //pair:(word, 1).
         JavaPairDStream<String, Integer> wordPairs = words.mapToPair((word) -> new Tuple2<>(word, 1));
