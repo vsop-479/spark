66bb83746720ba435b9ce7fc1ee4b06c4bb56e0c
zhouhui <zhouhui@360.net>
2019-08-13 16:31:46 +0800

    maxRatePerPartition.

--- test/src/main/java/streaming/fromKafka/KafkaConsumer.java
+++ test/src/main/java/streaming/fromKafka/KafkaConsumer.java
@@ -16,9 +16,7 @@ import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
-import scala.Tuple2;
 import streaming.toES.User;
-
 import java.util.*;
 
 public class KafkaConsumer {
@@ -27,6 +25,7 @@ public class KafkaConsumer {
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("SendRecord");
         conf.set("spark.streaming.backpressure.enabled", "true");
         conf.set("spark.streaming.receiver.maxRate", "1000");
+        conf.set("spark.streaming.kafka.maxRatePerPartition", "1000");
         conf.set("es.nodes", "10.74.17.26");
         conf.set("es.port", "9200");
         JavaStreamingContext ssc = new JavaStreamingContext(conf, Durations.seconds(2));
